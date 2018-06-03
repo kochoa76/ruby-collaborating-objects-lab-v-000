66234f4b5ca75bc04ac63ddbf62aaece58365bc9
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-03 17:33:29 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   end 
   
   def files
-    files = Dir.glob("#{path}*.mp3").map do |files|
+    files = Dir.glob("#{path}*.mp3").map{ |f| f.gsub("#{path}/", "") }
       
   end 
   
