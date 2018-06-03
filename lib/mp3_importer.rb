7db2e3e92bf06819c3f87f2922c0cc5b51edfdff
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-03 17:40:24 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,8 @@ class MP3Importer
   end 
   
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").count 
+      @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+
   end 
   
 end
