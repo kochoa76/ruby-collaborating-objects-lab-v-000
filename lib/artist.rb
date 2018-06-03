80b6f2a16e2a8f3f365af883b4ad8bd9fc099903
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-03 16:59:09 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist 
+  attr_accessor :name, :song 
+ @@all= []
+  def initialize(name)
+    @name = name
+    @songs=[]
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def add_song(song)
+    song = self.song 
+    @songs<<song 
+  end 
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song 
+  attr_accessor :name, :artist 
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def artist_name=(name)
+    self.artist=Artist.name 
+  end 
+
+  
+  def self.new_by_filename(filename)
+   artist,song = filename.split("-")
+
+    
+   
+  end 
+  
+end 
